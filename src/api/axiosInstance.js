46e47173a57b5491e1edeac85d4f658a4606e640
sumon72 <sumonfb72@gmail.com>
2025-11-13 23:58:58 +0600

    update axious

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_BASE_URL = "https://import-export-hub-server-two.vercel.app";
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://import-export-hub-server-two.vercel.app";
+//const API_BASE_URL = "http://localhost:4000";
 const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 5000,
